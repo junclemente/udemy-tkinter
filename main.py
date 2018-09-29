be3cc169fbd761b7f7b8a49b6dc9e2ef0882fd68
Jun C <kjstudios858@gmail.com>
2018-09-29 15:55:01 -0700

    refactor Add icon to window bar - Windows only. Doesn't appear to work in MacOS

--- main.py
+++ main.py
@@ -3,11 +3,14 @@ from tkinter import *
 # Create a tkinter window called root.
 root = Tk()
 
+# Define the size of the window
+root.geometry("300x300")
+
 # Provide a title for the window.
 root.title("Melody")
 
-# Define the size of the window
-root.geometry("300x300")
+# Add Icon to window bar
+root.iconbitmap(r'melody.ico')
 
 # Add a text label to the window
 text = Label(root, text="Let\'s make some noise!")
