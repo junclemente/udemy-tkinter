2f1c74a34297913e8b0f25f3339017a5c9b1462c
Jun C <kjstudios858@gmail.com>
2018-09-29 15:18:43 -0700

    refactor Add text label to window

--- main.py
+++ main.py
@@ -6,5 +6,9 @@ root = Tk()
 # Provide a title for the window.
 root.title("Melody")
 
+# Add a text label to the window
+text = Label(root, text="Let\'s make some noise!")
+text.pack()
+
 # Refreshes 'root' window
 root.mainloop()
