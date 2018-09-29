8d2094cbbce448739a202e47f8d881baa3ca9c51
Jun C <kjstudios858@gmail.com>
2018-09-29 15:20:25 -0700

    refactor Define size of window to be 300px square

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ root = Tk()
 # Provide a title for the window.
 root.title("Melody")
 
+# Define the size of the window
+root.geometry("300x300")
+
 # Add a text label to the window
 text = Label(root, text="Let\'s make some noise!")
 text.pack()
