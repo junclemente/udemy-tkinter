fc3b37de44d3c0c110a92ac607dc7d1bea7ea65e
Jun C <kjstudios858@gmail.com>
2018-09-30 22:53:04 -0700

    refactor Add label and radiobuttons

--- food_price.py
+++ food_price.py
@@ -8,6 +8,17 @@ root.resizable(width=False, height=False)
 root.configure(bg='gray77')
 
 
+label = Label(root, text="Choose an Item")
+label.place(x=95, y=5)
+
+r_btn = Radiobutton(root, text="Banana")
+r_btn.place(x=5, y=30)
+
+r_btn2 = Radiobutton(root, text="Apple")
+r_btn2.place(x=5, y=60)
+
+r_btn3 = Radiobutton(root, text="Orange")
+r_btn3.place(x=5, y=90)
 
 
 # Refreshes 'root' window and continues to run it
