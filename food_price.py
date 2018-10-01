1d0dbd376906e625f45200261f1032d46b79b393
Jun C <kjstudios858@gmail.com>
2018-10-01 10:53:48 -0700

    refactor Update background color of labels and button

--- food_price.py
+++ food_price.py
@@ -5,32 +5,34 @@ root = Tk()
 root.title("Food Price")
 root.geometry("300x200")
 root.resizable(width=False, height=False)
-root.configure(bg='gray77')
+color = 'gray77'
+root.configure(bg=color)
 
 
-label = Label(root, text="Choose an Item")
+label = Label(root, text="Choose an Item", bg=color)
 label.place(x=95, y=5)
 
-r_btn = Radiobutton(root, text="Banana")
+r_btn = Radiobutton(root, text="Banana", bg=color)
 r_btn.place(x=5, y=30)
 
-r_btn2 = Radiobutton(root, text="Apple")
+r_btn2 = Radiobutton(root, text="Apple", bg=color)
 r_btn2.place(x=5, y=60)
 
-r_btn3 = Radiobutton(root, text="Orange")
+r_btn3 = Radiobutton(root, text="Orange", bg=color)
 r_btn3.place(x=5, y=90)
 
 
 entry = Entry(root, width=22)
 entry.place(x=80, y=130)
 
-label_res = Label(root, text="Price")
+label_res = Label(root, text="Price", bg=color)
 label_res.place(x=5, y=130)
 
 entry_2 = Entry(root, width=22)
 entry_2.place(x=80, y=160)
 
-btn = Button(root, text="Cal")
+# highlightbackground
+btn = Button(root, text="Cal", highlightbackground=color)
 btn.place(x=5, y=160)
 
 # Refreshes 'root' window and continues to run it
