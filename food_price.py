d29e66b2e86300fb97adcb3d806a286b6fb8c907
Jun C <kjstudios858@gmail.com>
2018-10-01 11:44:31 -0700

    refactor Add cal_price function to calculate and display result

--- food_price.py
+++ food_price.py
@@ -8,7 +8,26 @@ root.resizable(width=False, height=False)
 color = 'gray77'
 root.configure(bg=color)
 
+# Declare variables
+BANANA = 5
+APPLE = 7
+ORANGE = 10
 v = IntVar()
+res = IntVar()
+
+
+def cal_price():
+    value = int(v.get())
+
+    if value == 0:
+        res.set(int(entry_2.get()) * BANANA)
+    elif value == 1:
+        res.set(int(entry_2.get()) * APPLE)
+    elif value == 2:
+        res.set(int(entry_2.get()) * ORANGE)
+    else:
+        pass
+
 
 label = Label(root, text="Choose an Item", bg=color)
 label.place(x=95, y=5)
@@ -23,7 +42,7 @@ r_btn3 = Radiobutton(root, text="Orange", bg=color, variable=v, value=2)
 r_btn3.place(x=5, y=90)
 
 
-entry = Entry(root, width=22)
+entry = Entry(root, width=22, textvariable=res)
 entry.place(x=80, y=130)
 
 label_res = Label(root, text="Price", bg=color)
@@ -32,7 +51,8 @@ label_res.place(x=5, y=130)
 entry_2 = Entry(root, width=22)
 entry_2.place(x=80, y=160)
 
-btn = Button(root, text="Cal", highlightbackground=color)
+btn = Button(root, text="Cal", highlightbackground=color,
+             command=lambda: cal_price())
 btn.place(x=5, y=160)
 
 # Refreshes 'root' window and continues to run it
