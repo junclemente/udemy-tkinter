fe11ce54e10251e5bd196e978a38e362f76acbbd
Jun C <kjstudios858@gmail.com>
2018-10-01 11:48:25 -0700

    docs Add comments to file

--- food_price.py
+++ food_price.py
@@ -8,7 +8,7 @@ root.resizable(width=False, height=False)
 color = 'gray77'
 root.configure(bg=color)
 
-# Declare variables
+# Declare variables, IntVar() is tkinter variable declaration
 BANANA = 5
 APPLE = 7
 ORANGE = 10
@@ -16,6 +16,7 @@ v = IntVar()
 res = IntVar()
 
 
+# Function that takes value from entry_2, calculates and updates res value
 def cal_price():
     value = int(v.get())
 
@@ -29,9 +30,11 @@ def cal_price():
         pass
 
 
+# Create text label at top of window
 label = Label(root, text="Choose an Item", bg=color)
 label.place(x=95, y=5)
 
+# Create and label radio buttons
 r_btn = Radiobutton(root, text="Banana", bg=color, variable=v, value=0)
 r_btn.place(x=5, y=30)
 
@@ -42,18 +45,21 @@ r_btn3 = Radiobutton(root, text="Orange", bg=color, variable=v, value=2)
 r_btn3.place(x=5, y=90)
 
 
+# Create and label 'Price' entry box
 entry = Entry(root, width=22, textvariable=res)
 entry.place(x=80, y=130)
 
 label_res = Label(root, text="Price", bg=color)
 label_res.place(x=5, y=130)
 
+# Create entry box and button that calls cal_price function
 entry_2 = Entry(root, width=22)
 entry_2.place(x=80, y=160)
 
 btn = Button(root, text="Cal", highlightbackground=color,
              command=lambda: cal_price())
 btn.place(x=5, y=160)
+
 
 # Refreshes 'root' window and continues to run it
 root.mainloop()
