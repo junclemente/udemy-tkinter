bcc51942ee0d2a678ae8d38a7648f5c097cd3c66
Jun C <kjstudios858@gmail.com>
2018-10-01 10:59:02 -0700

    refactor Add variables to radio buttons

--- food_price.py
+++ food_price.py
@@ -8,17 +8,18 @@ root.resizable(width=False, height=False)
 color = 'gray77'
 root.configure(bg=color)
 
+v = StringVar()
 
 label = Label(root, text="Choose an Item", bg=color)
 label.place(x=95, y=5)
 
-r_btn = Radiobutton(root, text="Banana", bg=color)
+r_btn = Radiobutton(root, text="Banana", bg=color, variable=v, value='Banana')
 r_btn.place(x=5, y=30)
 
-r_btn2 = Radiobutton(root, text="Apple", bg=color)
+r_btn2 = Radiobutton(root, text="Apple", bg=color, variable=v, value='Apple')
 r_btn2.place(x=5, y=60)
 
-r_btn3 = Radiobutton(root, text="Orange", bg=color)
+r_btn3 = Radiobutton(root, text="Orange", bg=color, variable=v, value='Orange')
 r_btn3.place(x=5, y=90)
 
 
@@ -31,7 +32,6 @@ label_res.place(x=5, y=130)
 entry_2 = Entry(root, width=22)
 entry_2.place(x=80, y=160)
 
-# highlightbackground
 btn = Button(root, text="Cal", highlightbackground=color)
 btn.place(x=5, y=160)
 
