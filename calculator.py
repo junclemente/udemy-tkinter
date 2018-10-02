9b1ba7863751286625c62de7ab042fb4ebddb0ed
Jun C <kjstudios858@gmail.com>
2018-10-02 13:27:51 -0700

    refactor Added functions for each provided word

--- calculator.py
+++ calculator.py
@@ -1,12 +1,10 @@
+import tkinter.messagebox
 from tkinter import *
 
 
-# Declare variables
+# Declare static variables
 COLOR = "gray55"
 COLOR2 = "gray88"
-num1 = IntVar()
-num2 = IntVar()
-result_value = IntVar()
 
 
 # Define main window
@@ -16,6 +14,10 @@ root.resizable(width=False, height=False)
 root.title("Simple Calculator")
 root.configure(bg=COLOR)
 
+# Declare Tk variables
+num1 = IntVar()
+num2 = IntVar()
+result_value = IntVar()
 
 # Define frames
 top_frame = Frame(root, width=400, height=40, bg=COLOR)
@@ -31,17 +33,69 @@ bottom_frame = Frame(root, width=400, height=40, bg=COLOR)
 bottom_frame.pack(side=TOP)
 
 
+# Define functions
+def errorMsg(msg):
+    if msg == 'error':
+        tkinter.messagebox.showerror("Error!", "Something went really wrong. ")
+    elif msg == 'divisionerror':
+        tkinter.messagebox.showerror("Division Error!",
+                                     "Cannot divide by 0. ")
+
+
+def plus():
+    try:
+        value = float(num1.get()) + float(num2.get())
+        result_value.set(value)
+
+    except:
+        errorMsg('error')
+
+
+def minus():
+    try:
+        value = float(num1.get()) - float(num2.get())
+        result_value.set(value)
+    except:
+        errorMsg('error')
+
+
+def multiply():
+    try:
+        value = float(num1.get()) * float(num2.get())
+        result_value.set(value)
+
+    except:
+        errorMsg('error')
+
+
+def divide():
+
+    if num2.get() == '0':
+        errorMsg('divisionerror')
+    elif num2.get() != '0':
+        try:
+            value = float(num1.get()) / float(num2.get())
+            result_value.set(value)
+
+        except:
+            errorMsg('error')
+
+
 # Define buttons
-btn_plus = Button(top3_frame, text=" + ", width=8, highlightbackground=COLOR)
+btn_plus = Button(top3_frame, text=" + ", width=8,
+                  highlightbackground=COLOR, command=lambda: plus())
 btn_plus.pack(side=LEFT, padx=5, pady=5)
 
-btn_minus = Button(top3_frame, text=" - ", width=8, highlightbackground=COLOR)
+btn_minus = Button(top3_frame, text=" - ", width=8,
+                   highlightbackground=COLOR, command=lambda: minus())
 btn_minus.pack(side=LEFT)
 
-btn_multiply = Button(top3_frame, text=" * ", width=8, highlightbackground=COLOR)
+btn_multiply = Button(top3_frame, text=" * ", width=8,
+                      highlightbackground=COLOR, command=lambda: multiply())
 btn_multiply.pack(side=LEFT)
 
-btn_divide = Button(top3_frame, text=" / ", width=8, highlightbackground=COLOR)
+btn_divide = Button(top3_frame, text=" / ", width=8,
+                    highlightbackground=COLOR, command=lambda: divide())
 btn_divide.pack(side=LEFT)
 
 
