364a046c65ffc0b09f9e13c3ed3c192d0c83b944
Jun C <kjstudios858@gmail.com>
2018-10-02 10:28:13 -0700

    initial commit

---
+++ calculator.py
@@ -0,0 +1,17 @@
+from tkinter import *
+
+
+# Declare variables
+COLOR = "gray55"
+COLOR2 = "gray88"
+
+# Define main window
+root = Tk()
+root.geometry("400x150")
+root.resizable(width=False, height=False)
+root.title("Simple Calculator")
+root.configure(bg=COLOR)
+
+
+# Execute window and refresh
+root.mainloop()
