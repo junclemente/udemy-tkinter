967a194e449d3c6bd60a3578be45309f1199320c
Jun C <kjstudios858@gmail.com>
2018-10-02 15:37:34 -0700

    refactor Define frames

--- emailer.py
+++ emailer.py
@@ -12,5 +12,18 @@ root.title("Email Sender")
 root.configure(bg=COLOR)
 
 
+# Define Frames
+top = Frame(root, width=800, height=50, bg="black")
+top.pack(side=TOP)
+
+bottom = Frame(root, width=800, height=50, bg="black")
+bottom.pack(side=BOTTOM)
+
+left = Frame(root, width=600, height=400, bg="red")
+left.pack(side=LEFT)
+
+right = Frame(root, width=200, height=400, bg="blue")
+right.pack(side=RIGHT)
+
 # Execute  root window
 root.mainloop()
