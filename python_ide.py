7aa57e0284c7c72423a48aac9dff640d529fdcad
Jun C <kjstudios858@gmail.com>
2018-10-01 23:36:13 -0700

    docs Add comments

--- python_ide.py
+++ python_ide.py
@@ -19,7 +19,7 @@ top.pack(side=TOP)
 bottom = Frame(root, width=800, height=450, bg=COLOR)
 bottom.pack(side=BOTTOM)
 
-
+# Define buttons
 btn_clear = Button(top, text="clear", highlightbackground=COLOR,
                    font=('arial', 25, 'bold'))
 btn_clear.pack(side=TOP)
@@ -28,8 +28,9 @@ btn_run = Button(top, text="run", highlightbackground=COLOR,
                  font=('arial', 25, 'bold'))
 btn_run.pack(side=TOP)
 
-
+# Define text box
 python_code = Text(bottom, font=('arial', 25, 'bold'), bg=COLOR2)
 python_code.pack(side=TOP)
+
 
 root.mainloop()
