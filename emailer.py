0bb553ccc2cf270d37164d9db54f103b46854f15
Jun C <kjstudios858@gmail.com>
2018-10-02 16:06:39 -0700

    refactor Add buttons to right frame

--- emailer.py
+++ emailer.py
@@ -16,14 +16,33 @@ root.configure(bg=COLOR)
 top = Frame(root, width=800, height=50, bg="black")
 top.pack(side=TOP)
 
-bottom = Frame(root, width=800, height=50, bg="black")
+bottom = Frame(root, width=800, height=50, bg="red")
 bottom.pack(side=BOTTOM)
 
-left = Frame(root, width=600, height=400, bg="red")
+left = Frame(root, width=600, height=400, bg="blue")
 left.pack(side=LEFT)
 
-right = Frame(root, width=200, height=400, bg="blue")
+right = Frame(root, width=200, height=400, bg=COLOR)
 right.pack(side=RIGHT)
+
+
+# Define buttons
+clear_to_btn = Button(right, text="Clear To", font=('arial', 20, 'bold'),
+                      highlightbackground=COLOR)
+clear_to_btn.pack(side=TOP, padx=5, pady=5)
+
+clear_from_btn = Button(right, text="Clear From", font=('arial', 20, 'bold'),
+                      highlightbackground=COLOR)
+clear_from_btn.pack(side=TOP, padx=5, pady=5)
+
+clear_all_btn = Button(right, text="Clear All", font=('arial', 20, 'bold'),
+                      highlightbackground=COLOR)
+clear_all_btn.pack(side=TOP, padx=5, pady=5)
+
+send_btn = Button(right, text="Send", font=('arial', 20, 'bold'),
+                      highlightbackground=COLOR)
+send_btn.pack(side=TOP, padx=5, pady=5)
+
 
 # Execute  root window
 root.mainloop()
