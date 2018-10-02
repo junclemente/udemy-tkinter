72ce32e8ac1b471d61a7618249f027fcfff6467b
Jun C <kjstudios858@gmail.com>
2018-10-01 23:33:10 -0700

    refactor Add buttons and textbox

--- python_ide.py
+++ python_ide.py
@@ -1,21 +1,35 @@
 from tkinter import *
 
 # Declare variables
-BGCOLOR = "gray"
-
+COLOR = "gray"
+COLOR2 = "gray88"
+# TOPCOLOR = "black"
+BOTTOMCOLOR="red"
 # Define window
 root = Tk()
 root.geometry("800x500")
 root.resizable(width=False, height=False)
 root.title("Run Python Code")
-root.configure(bg=BGCOLOR)
+root.configure(bg=COLOR)
 
 # Define frames
-top = Frame(root, width=800, height=50, bg="black")
+top = Frame(root, width=800, height=50, bg=COLOR)
 top.pack(side=TOP)
 
-bottom = Frame(root, width=800, height=450, bg="red")
+bottom = Frame(root, width=800, height=450, bg=COLOR)
 bottom.pack(side=BOTTOM)
 
+
+btn_clear = Button(top, text="clear", highlightbackground=COLOR,
+                   font=('arial', 25, 'bold'))
+btn_clear.pack(side=TOP)
+
+btn_run = Button(top, text="run", highlightbackground=COLOR,
+                 font=('arial', 25, 'bold'))
+btn_run.pack(side=TOP)
+
+
+python_code = Text(bottom, font=('arial', 25, 'bold'), bg=COLOR2)
+python_code.pack(side=TOP)
 
 root.mainloop()
