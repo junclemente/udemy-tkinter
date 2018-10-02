5a9248623c20502ff19eaa059239f600c48bc011
Jun C <kjstudios858@gmail.com>
2018-10-02 10:28:59 -0700

    refactor Add frames to root window

--- calculator.py
+++ calculator.py
@@ -12,6 +12,18 @@ root.resizable(width=False, height=False)
 root.title("Simple Calculator")
 root.configure(bg=COLOR)
 
+# Define frames
+top_frame = Frame(root, width=400, height=40, bg=COLOR)
+top_frame.pack(side=TOP)
+
+top2_frame = Frame(root, width=400, height=40, bg=COLOR)
+top2_frame.pack(side=TOP)
+
+top3_frame = Frame(root, width=400, height=40, bg=COLOR)
+top3_frame.pack(side=TOP)
+
+bottom_frame = Frame(root, width=400, height=40, bg=COLOR)
+bottom_frame.pack(side=TOP)
 
 # Execute window and refresh
 root.mainloop()
