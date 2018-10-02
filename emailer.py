e3ec78507a27c21c8968bdb978532f49211a64c2
Jun C <kjstudios858@gmail.com>
2018-10-02 15:33:40 -0700

    initial commit

---
+++ emailer.py
@@ -0,0 +1,16 @@
+from tkinter import *
+
+
+# Define static variables
+COLOR = 'gray55'
+
+# Define root window
+root = Tk()
+root.geometry("800x500")
+root.resizable(width=False, height=False)
+root.title("Email Sender")
+root.configure(bg=COLOR)
+
+
+# Execute  root window
+root.mainloop()
